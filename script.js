dbea5a4eff133463c24a12d21c2d516ecb51f84a
Dieniferlandpaz <dienepaz@hotmail.com>
2023-04-18 20:35:23 -0300

    Exercitando strings e arrays!

---
+++ script.js
@@ -0,0 +1,21 @@
+let array 
+console.log('a. ', array)
+
+array = null
+console.log ('b. ', array)
+
+array = [3,4,5,6,7,8,9,10,11,12,13]
+console.log('c. ',array.length) // 11 quantidade 
+
+let i = 0
+console.log('d. ',array[i])//3
+
+array[i+1] = 19
+console.log ('e. ', array)
+
+const valor = array[i+6]
+console.log ('f. ', valor)
+
+
+
+
