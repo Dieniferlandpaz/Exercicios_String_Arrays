bda42d05c6a5bef595c8c01d74f71c603af34713
Dieniferlandpaz <dienepaz@hotmail.com>
2023-04-18 21:59:59 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let array 
+/*let array 
 console.log('a. ', array)
 
 array = null
@@ -14,7 +14,49 @@ array[i+1] = 19
 console.log ('e. ', array)
 
 const valor = array[i+6]
-console.log ('f. ', valor)
+console.log ('f. ', valor)*/
+
+/*const frase = prompt ('digite uma frase: ')
+console.log(frase.toUpperCase().replaceAll("A", "I"),frase.length)*/
+
+
+// 1. Faça um programa que permite ao usuário seu nome e seu e-mail. Em seguida, imprima no console a seguinte mensagem
+// O email 'emailDoUsuario' foi cadastrado com sucesso. Seja bem -vinda(a), nomeDoUsuario!
+
+/*let nome = prompt ("Digite seu nome: ")
+console.log (nome)
+let emailDoUsuario = prompt ("Digite seu email: ")
+console.log (emailDoUsuario)
+console.log( "O e-mail ", emailDoUsuario , "foi cadastrado com sucesso.Seja bem -vinda", nome)*/
+
+/*3. Faça um programa, seguindo os passos:
+    
+    a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`
+    
+    b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+    
+    c) Imprima o array no console
+    
+    d) Peça ao usuário que digite o *índice* de uma tarefa que ele já realizou: 0, 1 ou 2 
+    
+    e) Remova da lista o item de índice que o usuário escolheu.
+    
+    f) Imprima o array no console*/
+
+    let array = null
+    let listaDeTarefas = array
+    let tarefa1 = prompt ("Digite sua primeira tarefa: ")
+    let tarefa2 = prompt ("Digite sua segunda tarefa: ")
+    let tarefa3 = prompt ("Digite sua terceira tarefa: ")
+    listaDeTarefas = [tarefa1,tarefa2,tarefa3]
+    console.log(listaDeTarefas) 
+
+    let i = [0+2]
+    console.log (listaDeTarefas, array)
+
+
+    
+
 
 
 
